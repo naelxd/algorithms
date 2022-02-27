44f967229b9f80ba9ef07ddd21b59f6b6e9f552f
nikitaosovskiy <nikitaosovskiy2005ok181@gmail.com>
2022-02-27 19:10:04 +0800

    Made bubble sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr) - 1, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+
+    return arr
